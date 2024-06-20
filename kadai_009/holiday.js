0defff015dafb287d5843976ae1b8bfd9af10a8b
yuisaaan <167851292+yuisaaan@users.noreply.github.com>
2024-06-20 12:09:29 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,15 @@
+//配列の宣言と値の代入を行う
+const holidays=["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
+
+//配列の値を出力する
+console.log(holidays);
+
+//for文.配列を順番に出力する
+for(let holidays =0;holidays < holidays.length;holidays += 1){
+  console.log(holidays);
+}
+
+//while文.配列を順番に出力する
+while(holidays <16){
+  console.log(holidays[i]);
+}
